552e068ca93f72ecea6422ffc436daaaced44358
kujbusgyorgy <kujbusgyorgy10@gmail.com>
2026-02-09 12:17:58 +0100

    1. hét - feladat 2-3

---
+++ App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Kujbus Gyorgy");
+        System.out.println("DMLPNR");
+        System.out.println("A kedvenc idezetem: Soha ne add fel!");
+    }
+}
+
+/*
+3. feladat – Koncepcionális tervezés
+
+Választott objektum: Nyomtató
+
+Class neve: Printer
+
+Adatok (attribútumok):
+- márka
+- modell
+- tintaszint
+- beVanKapcsolva
+- vanPapír
+
+Metódusok (műveletek):
+- bekapcsol()
+- kikapcsol()
+- nyomtat()
+- tintátCserél()
+- papírtTölt()
+*/
