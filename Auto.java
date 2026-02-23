53a237a7fe4bdf6ab67209f667d3cef6c825e931
Kujbus Gyorgy <kujbusgyorgy10@gmail.com>
2026-02-23 10:04:35 +0100

    Autó feladat megoldása

---
+++ Auto.java
@@ -0,0 +1,32 @@
+public class Auto {
+    String marka;
+    String model;
+    int evjarat;
+
+    public Auto(String marka, String model, int evjarat){
+
+        this.marka=marka;
+        this.model=model;
+        this.evjarat=evjarat;
+    }
+    
+
+    public void displayInfo(){
+
+        System.out.println("Márka: " +marka);
+        System.out.println("Modell: "+model);
+        System.out.println("Evjarat: "+evjarat);
+    }
+
+    public void startEngine(){
+        System.out.println("Az auto indul");
+    }
+
+    public void stopEngine()
+    {
+        System.out.println("Az auto megall.");
+    }
+
+
+
+}
